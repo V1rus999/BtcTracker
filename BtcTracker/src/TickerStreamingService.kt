381e3b833f16d7091443146dde5a8ee4c209d9b8
johannesC <JohannesC@k2.com>
2017-09-12 16:16:40 +0200

    master_Added a robot which moves the mouse on each run to keep screensaver from running. Added the manifest again to make the jar runnable.

--- BtcTracker/src/TickerStreamingService.kt
+++ BtcTracker/src/TickerStreamingService.kt
@@ -2,9 +2,13 @@ import markets.Ticker
 import markets.crypto_exchanges.CryptoExchange
 import markets.fiat_exchanges.FiatExchange
 import printer.TickerPrinter
+import java.awt.Robot
+import java.util.*
 import java.util.concurrent.Executors
 import java.util.concurrent.ScheduledExecutorService
 import java.util.concurrent.TimeUnit
+import java.awt.MouseInfo
+
 
 /**
  * Created by johannesC on 2017/09/03.
@@ -14,6 +18,7 @@ class TickerStreamingService(private val writer: TickerPrinter,
                              private val cryptoExchanges: ArrayList<CryptoExchange>) {
 
     private var scheduler: ScheduledExecutorService? = null
+    private val robot = Robot()
 
     fun startDownloadingTickerData() {
         scheduler = Executors.newSingleThreadScheduledExecutor()
@@ -31,6 +36,11 @@ class TickerStreamingService(private val writer: TickerPrinter,
             writeToFile(tickers)
             println(tickers)
             println("Press 1 to quit")
+
+            val pObj = MouseInfo.getPointerInfo().location
+            robot.mouseMove(pObj.x + 1, pObj.y + 1)
+            robot.mouseMove(pObj.x - 1, pObj.y - 1)
+
         }, 0, 15, TimeUnit.MINUTES)
     }
 
